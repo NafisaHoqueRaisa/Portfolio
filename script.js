8c6f27ea2656d88b4911bd35d5908f33a8eb2607
NafisaHoqueRaisa <cp2576284@gmail.com>
2025-12-06 11:26:12 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const sections = document.querySelectorAll(".section");
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("fade-in");
+        }
+    });
+},{threshold:0.2});
+
+sections.forEach(sec=> observer.observe(sec));
